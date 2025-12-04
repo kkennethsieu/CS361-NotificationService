c784a392abd681323d1bf62445d7307b84890615
kkennethsieu <kkennethsieu@gmail.com>
2025-12-03 18:33:01 -0800

    dockerfile

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use("/notify", notificationRoutes);
 
 const PORT = process.env.NOTIFY_PORT || 8030;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on ${PORT}`);
 });
 
